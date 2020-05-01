89dd55696f090f6d14a3bc2fbcb86885b0f68efc
Aswin Vijayakumar <aswinvk28@gmail.com>
2020-05-01 14:17:00 +0100

    caffe2 onnx update with argparse

--- caffe2ToONNX.py
+++ caffe2ToONNX.py
@@ -2,25 +2,39 @@ import onnx
 import caffe2.python.onnx.frontend
 from caffe2.proto import caffe2_pb2
 
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--predict_net', required=True)
+    parser.add_argument('--init_net', required=True)
+    parser.add_argument('--predict_net_name', required=True)
+    parser.add_argument('--init_net_name', required=True)
+    parser.add_argument('--onnx_file', required=True)
+
+    return parser.parse_args()
+
 if __name__ == "__main__":
+
+    args = parse_args()
 
     # We need to provide type and shape of the model inputs, 
     # see above Note section for explanation
     data_type = onnx.TensorProto.FLOAT
-    data_shape = (1, 3, 227, 227)
+    data_shape = (1, 3, 224, 224)
     value_info = {
         'data': (data_type, data_shape)
     }
 
     predict_net = caffe2_pb2.NetDef()
-    with open('predict_net.pb', 'rb') as f:
+    with open(args.predict_net, 'rb') as f:
         predict_net.ParseFromString(f.read())
-        predict_net.name = "squeezenet"
+        predict_net.name = args.predict_net_name
 
     init_net = caffe2_pb2.NetDef()
-    with open('init_net.pb', 'rb') as f:
+    with open(args.init_net, 'rb') as f:
         init_net.ParseFromString(f.read())
-        init_net.name = "squeezenet"
+        init_net.name = args.init_net_name
 
     onnx_model = caffe2.python.onnx.frontend.caffe2_net_to_onnx_model(
         predict_net,
@@ -30,4 +44,4 @@ if __name__ == "__main__":
 
     onnx.checker.check_model(onnx_model)
 
-    onnx.save(onnx_model, "squeezenet.onnx")
+    onnx.save(onnx_model, args.onnx_file)
